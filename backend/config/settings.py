30eac1b11b0a1bed4f24e98c14d86274ba7cdf73
WchrpWwtk <wchrp.wwtk@gmail.com>
2026-05-14 15:46:09 +0700

    feat: replace JWT authentication class with CookieJWTAuthentication in settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ CORS_ALLOWED_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "accounts.authentication.CookieJWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
