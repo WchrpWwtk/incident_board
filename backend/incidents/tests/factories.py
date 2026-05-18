a62a2b1e928b05c2b1de6bef404f20ef073e72f9
WchrpWwtk <wchrp.wwtk@gmail.com>
2026-05-18 15:12:25 +0700

    feat: add skip_postgeneration_save option to UserFactory in test suite

--- backend/incidents/tests/factories.py
+++ backend/incidents/tests/factories.py
@@ -9,6 +9,7 @@ User = get_user_model()
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
+        skip_postgeneration_save = True
 
     username = factory.Sequence(lambda n: f"user{n}")
 
