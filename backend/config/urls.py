4f00b2fc2d408c412c1abf2c1be12b2021ec8932
WchrpWwtk <wchrp.wwtk@gmail.com>
2026-05-25 11:53:44 +0700

    chore: add spacing for readability in urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,8 +21,10 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path, include
 
+
 def healthz(request):
     return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("healthz/", healthz, name="healthz"),
